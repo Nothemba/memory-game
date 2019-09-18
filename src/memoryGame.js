452fcd40e42ab635e337b7006c6d25a25c55a0c8
Nothemba Tsheme <tnothemba@gmail.com>
2019-09-18 10:20:49 +0200

    renamed speckfile, added a timer, a winner message and auto refresh after playing a game

--- src/memoryGame.js
+++ src/memoryGame.js
@@ -1,9 +1,11 @@
-//creating an array from DOM elements
 const cards = document.querySelectorAll('.card');
 
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let matchCounter = 0;
+var mins = 1;  
+var secs = mins * 60;
 
 // Flipping the cards onclick
 function flipCard() {
@@ -29,8 +31,21 @@ function flipCard() {
 }
 //Check for matching cards
 function checkForMatch() {
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-  isMatch ? disableCards() : unflipCards();
+let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+ if (isMatch){
+     matchCounter += 1;
+     disableCards();
+     if(matchCounter == (cards.length/2)){
+         alert("Congratulations! You won!")
+         location.reload();
+     }
+     else if(matchCounter <(cards.length/2 && secs < 0)){
+      countdown(); 
+     }
+ }
+ else {
+     unflipCards();
+ }
 }
 //Disable the cards that are aready clicked on
 function disableCards() {
@@ -65,3 +80,44 @@ function resetBoard() {
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
+ 
+//countdown function is evoked when page is loaded 
+function countdown() { 
+    setTimeout('Decrement()', 60); 
+} 
+
+//Decrement function decrement the value. 
+function Decrement() { 
+    if (document.getElementById) { 
+        minutes = document.getElementById("minutes"); 
+        seconds = document.getElementById("seconds"); 
+        if (seconds < 59) { 
+            seconds.value = secs; 
+        }  
+        else { 
+            minutes.value = getminutes(); 
+            seconds.value = getseconds(); 
+        } 
+        
+        if (mins < 0) { 
+            alert('Game Over, You lost'); 
+            minutes.value = 0; 
+            seconds.value = 0; 
+            location.reload();
+        } 
+        else { 
+            secs--; 
+            setTimeout('Decrement()', 1000); 
+        } 
+    } 
+} 
+
+function getminutes() {  
+    mins = Math.floor(secs / 60); 
+    return mins; 
+} 
+
+function getseconds() {
+    return secs - Math.round(mins * 60); 
+}
+
